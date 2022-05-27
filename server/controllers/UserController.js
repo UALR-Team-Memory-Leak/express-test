8903a8757a60f5881846396ebfd2c52bd3bcea58
JustinDady7 <56369832+JustinDady7@users.noreply.github.com>
2022-05-26 20:34:52 -0500

    Attempted to get things to work

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,11 +1,11 @@
 'use strict'
 
-class UserController {
-    async login ({request, auth, response}) {
-        const { email, password } = request.all();
-        const token = await auth.attempt(email, password);
-        return response.status(200).json({token});
-    }
+const login = async ({req, res}) => {
+    console.log("hi")
+    const { email, password } = req.body;
+    //const token = await auth.attempt(email, password);
+    //return res.status(200).json({token});
+    return {email, password};
 }
 
-module.exports = UserController
+module.exports = {login};
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const router = express.Router();
+router.use(express.json());
 
-const Route = use('Route')
-
-const userController = require('../controllers/userController');
+const userController = require('../controllers/UserController');
 
 
-Route.post('/login', userController.login);
+router.post('/login', function (req, res) {
+    const { email, password } = req.body;
+    res.json({email, password})
+});
 
 module.exports = router;
